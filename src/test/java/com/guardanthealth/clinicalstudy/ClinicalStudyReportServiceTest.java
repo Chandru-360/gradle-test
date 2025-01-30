c1dbf086143b2cd68fabc2df5152c1b03e6693d3
Chandru-360 <150351350+Chandru-360@users.noreply.github.com>
2025-01-30 10:36:38 +0530

    Update ClinicalStudyReportServiceTest.java

--- src/test/java/com/guardanthealth/clinicalstudy/ClinicalStudyReportServiceTest.java
+++ src/test/java/com/guardanthealth/clinicalstudy/ClinicalStudyReportServiceTest.java
@@ -1,1 +1,25 @@
- 
+package com.guardanthealth.clinicalstudy;
+
+import com.google.inject.Inject;
+import com.guardanthealth.core.io.persist.PersistService;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class ClinicalStudyReportServiceTest {
+
+    @Inject
+    private PersistService persistService; // Injected dependency
+
+    @BeforeSuite
+    public void beforeSuite() {
+        // Initialize services (this is where the error occurs)
+        if (persistService == null) {
+            throw new RuntimeException("Initialization error!");
+        }
+    }
+
+    @Test
+    public void testSomething() {
+        // Test logic here
+    }
+} 
