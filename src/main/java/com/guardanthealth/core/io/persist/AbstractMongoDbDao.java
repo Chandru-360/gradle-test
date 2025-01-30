844696b6d7dc1e3abbfeccfaefdf39ae6d860c01
Chandru-360 <150351350+Chandru-360@users.noreply.github.com>
2025-01-30 10:36:00 +0530

    Update AbstractMongoDbDao.java

--- src/main/java/com/guardanthealth/core/io/persist/AbstractMongoDbDao.java
+++ src/main/java/com/guardanthealth/core/io/persist/AbstractMongoDbDao.java
@@ -1,1 +1,26 @@
- 
+package com.guardanthealth.core.io.persist;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+public abstract class AbstractMongoDbDao {
+
+    private Datastore datastore;
+
+    public void start() {
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        Morphia morphia = new Morphia();
+        
+        // This line causes the error because databaseName is null
+        MongoDatabase database = mongoClient.getDatabase(null); // <-- Error here
+        
+        datastore = morphia.createDatastore(mongoClient, database.getName());
+        datastore.ensureIndexes();
+    }
+
+    public Datastore getDatastore() {
+        return datastore;
+    }
+} 
